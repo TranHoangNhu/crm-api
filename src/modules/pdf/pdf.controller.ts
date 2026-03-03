4335258a132566c4429c325534424bb11f713162
Tran Hoang Nhu <tran.hoang.nhu.1997@gmail.com>
2026-03-03 10:13:49 +0700

    Make PDF endpoints public (no auth for window.open)

--- src/modules/pdf/pdf.controller.ts
+++ src/modules/pdf/pdf.controller.ts
@@ -1,36 +1,29 @@
 import { Controller, Get, Query, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { PdfService } from './pdf.service';
+import { Public } from '../../common/decorators/public.decorator';
 
+@Public()
 @Controller('pdf')
 export class PdfController {
   constructor(private pdfService: PdfService) {}
 
   @Get('quote')
-  async getQuotePdf(
-    @Query('id') quoteId: string,
-    @Res() res: Response,
-  ) {
+  async getQuotePdf(@Query('id') quoteId: string, @Res() res: Response) {
     const html = await this.pdfService.generateQuotePdf(quoteId);
     res.setHeader('Content-Type', 'text/html; charset=utf-8');
     res.send(html);
   }
 
   @Get('contract')
-  async getContractPdf(
-    @Query('id') contractId: string,
-    @Res() res: Response,
-  ) {
+  async getContractPdf(@Query('id') contractId: string, @Res() res: Response) {
     const html = await this.pdfService.generateContractPdf(contractId);
     res.setHeader('Content-Type', 'text/html; charset=utf-8');
     res.send(html);
   }
 
   @Get('handover')
-  async getHandoverPdf(
-    @Query('id') contractId: string,
-    @Res() res: Response,
-  ) {
+  async getHandoverPdf(@Query('id') contractId: string, @Res() res: Response) {
     const html = await this.pdfService.generateHandoverPdf(contractId);
     res.setHeader('Content-Type', 'text/html; charset=utf-8');
     res.send(html);
